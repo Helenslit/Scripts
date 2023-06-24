9f2c43bc15abb306cea57140dce44af4c2f82bf0
Helen Djurdjevac <38733413+Helenslit@users.noreply.github.com>
2023-06-24 12:16:45 +0200

    Update en.js

---
+++ js/clients/lang/en.js
@@ -0,0 +1,457 @@
+//english translation
+//the h1 is for other pages except index
+var main = {
+title:"Index Page Reliable Services in London | cluckSouxs",
+desc:"we aee the most reliable serbicex",
+h21:"",
+h22:"",
+h23:"",
+h24:"",
+twitcard:'hello twitcard',
+twititle:'hello from title',
+twitdesc:'hello from desc',
+twitimg:'',
+twitsite:'',
+ogtitle:'',
+ogdesc:'',
+ogimg:'',
+ogurl:'',
+ogsite:'' ,
+ogtype:''
+},
+  
+about ={
+title:"about us Reliable Services in London | About",
+desc:"we aee the most About",
+h1:"",
+h21:"",
+h22:"",
+h23:"",
+h24:"",
+twitcard:'hello twitcard | about',
+twititle:'hello from title',
+twitdesc:'hello from desc',
+twitimg:'',
+twitsite:'',
+ogtitle:'',
+ogdesc:'',
+ogimg:'',
+ogurl:'',
+ogsite:'' ,
+ogtype:''
+},
+  
+contact= {
+title:"Contact Services in London | contact",
+desc:"we aee the most contact",
+h1:"",
+h21:"",
+h22:"",
+h23:"",
+h24:"",
+twitcard:'hello twitcard | contact',
+twititle:'hello from title',
+twitdesc:'hello from desc',
+twitimg:'',
+twitsite:'',
+ogtitle:'',
+ogdesc:'',
+ogimg:'',
+ogurl:'',
+ogsite:'' ,
+ogtype:''
+},
+
+themes = {
+title:"Templates Services in London | product",
+desc:"we aee the most product",
+h1:"",
+h21:"",
+h22:"",
+h23:"",
+h24:"",
+twitcard:'hello twitcard',
+twititle:'hello from title',
+twitdesc:'hello from desc',
+twitimg:'',
+twitsite:'',
+ogtitle:'',
+ogdesc:'',
+ogimg:'',
+ogurl:'',
+ogsite:'' ,
+ogtype:''
+},
+
+fpage = {
+title:"Faqs Services in London | fag oage",
+desc:"we aee the most faqs",
+h1:"",
+h21:"",
+h22:"",
+h23:"",
+h24:"",
+twitcard:'hello twitcard',
+twititle:'hello from title',
+twitdesc:'hello from desc',
+twitimg:'',
+twitsite:'',
+ogtitle:'',
+ogdesc:'',
+ogimg:'',
+ogurl:'',
+ogsite:'' ,
+ogtype:''
+}
+;
+
+  
+
+const en = {
+//schema
+pagename:"Drip Design Studio",
+webdesc:"Express Logo Design in Nigeria | Graphic Design Services",
+webDescRs:"Express Logo Design in Nigeria | Graphic Design Services",
+//schema
+companyemail:"dripdesignstudio@gmail.com",
+domainame:"DripDesign",
+companyname:"Drip Design Studio",
+domain:"https://dripdesignstudio.net",
+about:"About",
+contact:"Contact",
+faqs:"Faqs",
+temp:"Templates",
+indexh1:"Minimalistic Logo Design",
+indexpara:"We offer customizable and ready-to-use logo for small businesses and individual who are not looking for a unique creative design within 48 hours.No Tedious processes involved",
+howbtn:"How Does it Work?",
+offerbtn:"View Templates",
+customhead:"Logo Customisability",
+custompara:"We understand your need to to stand in the crowd, so we made it possible to customize some part of the logo",
+customs:[
+  {
+custom:"Customizable Color"
+    },
+{
+custom:"Customisable Text"
+},
+{
+custom:"Customisable Company/Person Name"
+  },
+  {
+custom:"Customisable Phrases/Motto/Slogans"
+  }
+],
+process:"How Does it Work?",
+process1:"Click on the template link, select any logo of your choice and follow the instruction on the popup modal",
+process2:"Upon receiving your request, we will send you a confirmation link and instructions of 50% advanced payment via Facebook Messenger",
+process3:"Within 4-6 hours you will get a link to preview your logo in 4 different versions",
+process4:"if satisfied with the design and no changes are needed, you will proceed to pay the remaining balance. Finally, we will email you the logo as soon as our account is credited",
+availtemp:"Available Templates",
+thinkus:"What Others Think About Us",
+person1:"The stress-free services, high quality and responsive logo",
+person2:"I received my Logo within 2 hours, make una try am out.I am very satisfied with this company",
+person3:"I am rating this company 3 stars because i received my logo beyond the given deadline and am sure that my bank wasn't an issue.Aside from that the logo is good and of great quality",
+person4:"Lovely logo, received mine within 6 hours.It's been an amazing experience, well-deserved 5 stars",
+footop:"All right reserved by Drip |",
+disclaimer:"Disclaimer - Some part of our site, materials, libraries and resources may have been obtained from or maintained by a third party source.",
+readMore:"read our privacy policy for more",
+morebtn:"Load More",
+allCaught:"you are all caught up",
+//note : 
+abtop:"About Drip Design Studio",
+abtmid:"About Us",
+abtext:"We are an international Logo Design Studio based in Serbia. We offer prebuilt logo templates for small businesses and individuals who have limited time and resource to go for specially designed logo. Our goal is to build a high quality and affordable logo that would serve any individual from all walks of life. Owning a logo doesn't have to costs a fortune.",
+bizmotto:"Our Core Business Culture",
+mottos:[
+  {
+motto:"Reliability"
+  },
+  {
+motto:"Quality"
+    },
+  {
+motto:"Professionalism"
+    },
+  {
+motto:"Precision"
+  },
+  {
+  motto:"Time is Money"
+    },
+{
+  motto:"We Love What We Do"
+    }
+  ],
+  progress:"Work in Progress",
+  progtext:"We Strive continously for our product diversity and we have made it a goal to add 10+ templates per day.",
+  contpara:"For all questions and enquiries about Custom Logo, contact us by clicking on the Blue Facebook Messenger Icon below. We respond within 24 hours.",
+    contmid:"Contact Us",
+  contop:"Contact Drip Design Studio",
+    privmid:"Privacy Policy",
+  form:"How To Contact Us",
+  answered:"Questions & Answers",
+  faqtop:"Most Popular Questions",
+  faqmid:"FAQS",
+  temptop:"Ready to Use Logo Design",
+  tempmid:"Templates",
+  tempoffer:"Available Templates",
+  footop: "Alright Reserved by |",
+
+  //modal form
+  loadtext:"Load More",
+  total:"Total",
+  submit:"Submit Request",
+  version:"Version",
+  logoOrder:"Ordering Logo",
+  labicon:"Icon",
+  labcontact:"Phone Number",
+  labinit:"Business Initials",
+  labemail:"Email",
+  labBiztext:"Business Name",
+  labmotto:"Business Motto",
+  biztext: "Ex. Extreme Design Studio",
+  bizinit: "Initials Ex- MP, CD",
+  bizphone:"Contact Number",
+  bizslogan:"Business Motto",
+  bizemail:"Contact Email",
+  cardetails:"Details",
+  field:"field",
+  empty:"empty"
+  //modal form ends
+}
+
+var navUrl = {
+home:"index.html",
+about:"about.html",
+faq:"frequently-asked-questions.html",
+contact:"contact.html",
+temp:"logo-templates.html",
+policy:"privacy-policy.html"
+}
+
+var accordionEn = [
+  {
+  head:"english what we Do 1",
+  text:"1.this is what am saying yesh all"
+  }, 
+    {
+  head:"what we Do 2",
+  text:"2.this is what am saying yesh all"
+  }, 
+    {
+  head:"what we Do!3",
+  text:"3.this is what am saying yesh all"
+  }, 
+
+      {
+  head:"what we Do 4",
+  text:"4.this is what am saying yesh all"
+  }, 
+];
+
+
+function inner(name,value){
+document.querySelector(name).innerHTML = value;
+}
+
+function pholder(name,value) {
+document.querySelector(name).placeholder = value;
+}
+
+
+function aboutEn() {
+inner('#about-ptag', en.abtext);
+inner('#about-h1', en.abtmid);
+inner('#about-h3', en.abtop);
+inner('#bizmotto', en.bizmotto);
+inner('#progress', en.progress);
+inner('#progress-text', en.progtext);
+//mottos
+for(let i=0; i<en.mottos.length; i++){
+  console.log(en.mottos[i].motto);
+  var mottoCards = `
+<div class="motto-card">
+<h2>${en.mottos[i].motto}</h2>
+</div>
+`;
+document.querySelector('.product-card-wrap').innerHTML += mottoCards;
+}
+}
+
+
+function contactEn() {
+  inner('#about-h1', en.contmid);
+  inner('#about-h3', en.contop);
+  inner('#about-ptag', en.contpara);
+}
+//privacy h1 tag
+function privacyEn() {
+  inner('#about-h1', en.privmid);
+}
+
+var faqsEn = ()  => {
+  inner('#about-h1', en.faqmid);
+  inner('#about-h3', en.faqtop);
+  inner('#bizmotto', en.answered);
+}
+
+function productEn() {
+inner('#about-h1', en.tempmid);
+inner('#about-h3', en.temptop);
+inner('#tempoffer', en.tempoffer);
+inner('#loadtext', en.loadtext);
+inner('#total-counter', en.total);
+}
+
+function footEn() {
+inner('.copy', en.footop);
+inner('.read-more', en.readMore);
+inner('.disclaimer', en.disclaimer);
+}
+
+
+function indexEn(){
+//view template button & links
+document.querySelectorAll('.see-offers').forEach((btn) => {
+   btn.innerHTML = en.offerbtn;
+  btn.parentNode.href = navUrl.temp;
+  //console.log(btn.parentNode);
+});
+inner("#customize-head", en.customhead);
+  inner(".customize-para", en.custompara);
+  inner("#index-head", en.indexh1);
+  inner("#think-head", en.thinkus);
+  inner("#process-head", en.process);
+  inner("#index-para", en.indexpara);
+
+  //customisability
+for(let i = 0; i<en.customs.length; i++){
+  let proDivs =`
+     <li><i data-feather="check-square"></i>
+  ${en.customs[i].custom}
+     </li>`;
+document.querySelector('#customize-ul').innerHTML += proDivs;
+  }
+}
+
+
+function navLeft(){
+document.querySelector('.nav-left').children[1].firstElementChild.innerHTML = en.temp;
+document.querySelector('.nav-left').children[2].firstElementChild.innerHTML = en.about;
+    document.querySelector('.nav-left').children[3].firstElementChild.innerHTML = en.faqs;
+}
+
+
+function policyEn(){
+let policy = `<h1>Cookie Policy for <span class="company">${en.domainame}</h1>
+<p>This is the Cookie Policy for <span class="company">${en.companyname}</span>, accessible from <span class="company">${en.domain}
+</span></p>
+<p><strong>What Are Cookies</strong></p>
+
+<p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
+
+<p><strong>How We Use Cookies</strong></p>
+
+<p>We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
+
+<p><strong>Disabling Cookies</strong></p>
+
+<p>You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies. This Cookies Policy was created with the help of the <a href="https://www.cookiepolicygenerator.com/cookie-policy-generator/">Cookies Policy Generator</a>.</p>
+<p><strong>The Cookies We Set</strong></p>
+
+<ul>
+
+
+<li>
+    <p>Forms related cookies</p>
+    <p>When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.</p>
+</li>
+
+</ul>
+
+<p><strong>Third Party Cookies</strong></p>
+
+<p>In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</p>
+
+<ul>
+
+<li>
+    <p>This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.</p>
+    <p>For more information on Google Analytics cookies, see the official Google Analytics page.</p>
+</li>
+
+<li>
+    <p>Third party analytics are used to track and measure usage of this site so that we can continue to produce engaging content. These cookies may track things such as how long you spend on the site or pages you visit which helps us to understand how we can improve the site for you.</p>
+</li>
+
+<li>
+    <p>From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most.</p>
+</li>
+
+<li>
+    <p>As we sell products it's important for us to understand statistics about how many of the visitors to our site actually make a purchase and as such this is the kind of data that these cookies will track. This is important to you as it means that we can accurately make business predictions that allow us to monitor our advertising and product costs to ensure the best possible price.</p>
+</li>
+
+<li>
+    <p>The Google AdSense service we use to serve advertising uses a DoubleClick cookie to serve more relevant ads across the web and limit the number of times that a given ad is shown to you.</p>
+    <p>For more information on Google AdSense see the official Google AdSense privacy FAQ.</p>
+</li>
+
+<li>
+    <p>We use adverts to offset the costs of running this site and provide funding for further development. The behavioural advertising cookies used by this site are designed to ensure that we provide you with the most relevant adverts where possible by anonymously tracking your interests and presenting similar things that may be of interest.</p>
+</li>
+
+
+<li>
+    <p>Several partners advertise on our behalf and affiliate tracking cookies simply allow us to see if our customers have come to the site through one of our partner sites so that we can credit them appropriately and where applicable allow our affiliate partners to provide any bonus that they may provide you for making a purchase.</p>
+</li>
+
+<li>
+    <p>We also use social media buttons and/or plugins on this site that allow you to connect with your social network in various ways. For these to work the following social media sites including; {List the social networks whose features you have integrated with your site?:12}, will set cookies through our site which may be used to enhance your profile on their site or contribute to the data they hold for various purposes outlined in their respective privacy policies.</p>
+</li>
+
+</ul>
+
+<p><strong>More Information</strong></p>
+
+<p>Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site.</p>
+
+<p>For more general information on cookies, please read <a href="https://www.cookiepolicygenerator.com/sample-cookies-policy/">the Cookies Policy article</a>.</p>
+
+<p>However if you are still looking for more information then you can contact us through one of our preferred contact methods:</p>
+<ul>
+<li>Email: 
+<span class="cookie-email">
+${en.companyemail}</li>
+</span>
+</ul>`;
+document.querySelector('.policy-text').innerHTML = policy;
+//contact email
+document.querySelector('.cookie-email').onclick = (e) => {
+window.location.target = "blank";
+window.location.href = `mailto:${en.companyemail}`;
+e.preventDefault();
+}
+}
+
+
+function modalFormEn(){
+inner(".send-form", en.submit);
+inner(".logo-order", en.logoOrder);
+inner(".send-form", en.submit);
+inner(".version", en.version);
+
+inner(".label-icon", en.labicon);
+inner(".label-email", en.labemail);
+inner(".label-biztext", en.labBiztext);
+inner(".label-email", en.labemail);
+inner(".label-init", en.labinit);
+inner(".label-motto", en.labmotto);
+inner(".label-contact", en.labcontact);
+
+pholder("#pslogan", en.bizslogan);
+pholder("#bizphone", en.bizphone);
+pholder("#bizemail", en.bizemail);
+pholder("#bizname", en.biztext);
+pholder("#bizinitial", en.bizinit);
+}
